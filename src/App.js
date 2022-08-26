2e7dc97cf2bc12151520eeeaff2dc7d995205fd3
Steven <ljaja7198@gmail.com>
2022-08-26 13:57:20 +0700

    feat: part 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 function App() {
 	return (
-		<div>
-			<h1 className="text-blue-500">Hello world</h1>
+		<div className="layout pt-8">
+			<div className="max-w-[26rem] mx-auto">
+				<h1 className="text-blue-500 bg-red-200">Hello world</h1>
+				<p>Hello world</p>
+				<div className="flex gap-1">
+					<div className="w-4 h-4 rounded-lg bg-bg border border-primary"></div>
+					<div className="w-4 h-4 rounded-lg bg-primary"></div>
+					<div className="w-4 h-4 rounded-lg bg-done"></div>
+					<div className="w-4 h-4 rounded-lg bg-done-secondary"></div>
+					<div className="w-4 h-4 rounded-lg bg-ongoing"></div>
+					<div className="w-4 h-4 rounded-lg bg-ongoing-secondary"></div>
+					<div className="w-4 h-4 rounded-lg bg-undone"></div>
+					<div className="w-4 h-4 rounded-lg bg-undone-secondary"></div>
+					<div className="w-4 h-4 rounded-lg bg-board"></div>
+					<div className="w-4 h-4 rounded-lg bg-delete"></div>
+				</div>
+			</div>
 		</div>
 	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
+		colors: {
+			bg: "rgb(var(--color-bg) / <alpha-value>)",
+			primary: "rgb(var(--color-primary) / <alpha-value>)",
+			done: "rgb(var(--color-done) / <alpha-value>)",
+			"done-secondary": "rgb(var(--color-done-secondary) / <alpha-value>)",
+			ongoing: "rgb(var(--color-ongoing) / <alpha-value>)",
+			"ongoing-secondary": "rgb(var(--color-ongoing-secondary) / <alpha-value>)",
+			undone: "rgb(var(--color-undone) / <alpha-value>)",
+			"undone-secondary": "rgb(var(--color-undone-secondary) / <alpha-value>)",
+			board: "rgb(var(--color-board) / <alpha-value>)",
+			delete: "rgb(var(--color-delete) / <alpha-value>)",
+		},
+		extend: {
+			fontFamily: {
+				sans: ["Poppins", "sans-serif"],
+			},
+		},
 	},
 	plugins: [],
 };
